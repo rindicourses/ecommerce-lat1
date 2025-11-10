82ddce23b6695056d7be30eb1253cc09f1fc4930
rindicourses <rindicourses@gmail.com>
2025-11-11 06:38:15 +0700

    feat(profile) conflict 2

--- profile.js
+++ profile.js
@@ -1,5 +1,4 @@
 console.log('profile is ready');
 console.log('main: update for rebase conflict');
-console.log('feature: update from rebase training');
-console.log('feature: update from rebase trainingA');
+console.log('feature: update from rebase training - 2');
 
