804ce44e8094dc161d47a79f6e9e2be770ef6460
rindicourses <rindicourses@gmail.com>
2025-11-11 06:31:33 +0700

    feat(profile) change A

--- profile.js
+++ profile.js
@@ -1,4 +1,5 @@
 console.log('profile is ready');
 console.log('main: update for rebase conflict');
 console.log('feature: update from rebase training');
+console.log('feature: update from rebase trainingA');
 
